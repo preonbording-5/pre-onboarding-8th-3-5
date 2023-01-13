728d57a07c36bcd5662707fe7aaf286318125962
Seongtaek Hwang <hst1993@hanmail.net>
2023-01-13 20:22:35 +0900

    feat: 세션스토리지를 이용한 캐시 구현

---
+++ src/lib/apis/getCacheData.ts
@@ -0,0 +1,29 @@
+import { getSickData } from './getData';
+
+const maxLength = 50;
+
+export const getCacheData = async (keyWord: string) => {
+  if (sessionStorage.getItem(keyWord) === null) {
+    try {
+      const { data } = await getSickData(keyWord);
+      if (sessionStorage.length >= maxLength) {
+        sessionStorage.removeItem(sessionStorage.key(0) as string);
+        sessionStorage.setItem(keyWord, JSON.stringify(data));
+      } else {
+        sessionStorage.setItem(keyWord, JSON.stringify(data));
+      }
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    const data = sessionStorage.getItem(keyWord);
+    if (sessionStorage.length > maxLength) {
+      sessionStorage.removeItem(keyWord);
+      sessionStorage.setItem(keyWord, JSON.stringify(data));
+    } else {
+      sessionStorage.setItem(keyWord, JSON.stringify(data));
+    }
+    return JSON.parse(data as string);
+  }
+};
